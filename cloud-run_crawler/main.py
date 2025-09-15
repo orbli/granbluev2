7656a4c61ed56619f8437b66f1e4c617fa3c7b6f
orb li <mail@orbb.li>
2025-09-15 04:22:09 +0000

    feat: split crawler job into two tasks

--- cloud-run_crawler/main.py
+++ cloud-run_crawler/main.py
@@ -2,13 +2,17 @@ import fs_configs
 import get_personal_border
 import get_crew
 import datetime
+import os
 
 def main():
     ts = datetime.datetime.now()
     fs_configs.get_configs()
+    task_index = int(os.environ.get("CLOUD_RUN_TASK_INDEX", 0))
     try:
-        get_personal_border.get_personal_border(ts)
-        get_crew.get_crew(ts)
+        if task_index == 0:
+            get_personal_border.get_personal_border(ts)
+        elif task_index == 1:
+            get_crew.get_crew(ts)
         print('success')
     finally:
         fs_configs.set_cookie()
